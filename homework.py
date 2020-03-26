0fce827cd4beee333ce51ae16c2467a0f8116b18
leks20 <leha-spb@yandex.ru>
2020-03-27 00:25:03 +0300

    Second

--- homework.py
+++ homework.py
@@ -1,17 +1,24 @@
 import os
-import requests
-import telegram
 import time
+
 from dotenv import load_dotenv
+import requests
+from requests.exceptions import ConnectionError, \
+                                Timeout, RequestException, InvalidHeader, \
+                                InvalidURL, ProxyError, InvalidProxyURL
+import telegram
 
 load_dotenv()
 
-
-PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
+PRAKTIKUM_TOKEN = os.getenv("PRAKTIKUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-proxy = telegram.utils.request.Request(proxy_url='socks5://95.110.194.245:26518')
+proxy = telegram.utils.request.Request(
+    proxy_url='socks5://95.110.194.245:26518'
+    )
+
+bot = telegram.Bot(token=TELEGRAM_TOKEN, request=proxy)
 
 
 def parse_homework_status(homework):
@@ -19,22 +26,38 @@ def parse_homework_status(homework):
     if homework['status'] == 'rejected':
         verdict = 'К сожалению в работе нашлись ошибки.'
     else:
-        verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
+        verdict = 'Ревьюеру всё понравилось, \
+                    можно приступать к следующему уроку.'
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
 def get_homework_statuses(current_timestamp):
     url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
-    headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+    headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
     data = {
         'from_date': current_timestamp,
     }
-    homework_statuses = requests.get(url, params=data, headers=headers)
-    return homework_statuses.json()
+    try:
+        homework_statuses = requests.get(url, params=data, headers=headers)
+        return homework_statuses.json()
+    except ProxyError:
+        print("Ошибка прокси-сервера!")
+    except ConnectionError:
+        print("Возникла ошибка соединения! \
+            Проверьте Ваше подключение к интернету.")
+    except Timeout:
+        print("Время ожидания запроса истекло!")
+    except InvalidProxyURL:
+        print("Недопустимый URL-адрес прокси-сервера!")
+    except InvalidURL:
+        print("Недействительный URL-адрес!")
+    except InvalidHeader:
+        print("Недопустимое значение заголовка!")
+    except RequestException:
+        print("Что-то пошло не так...")
 
 
 def send_message(message):
-    bot = telegram.Bot(token=TELEGRAM_TOKEN, request=proxy)
     return bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
 
 
@@ -44,15 +67,30 @@ def main():
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
-            if new_homework.get('homeworks'):
-                send_message(parse_homework_status(new_homework.get('homeworks')[0]))
-            current_timestamp = new_homework.get('current_date')  # обновить timestamp
-            time.sleep(1200)  # опрашивать раз в 20 минут
+
+            if new_homework is None:
+                print('Через 1 минуту попробуем еще раз')
+                time.sleep(60)
+            else:
+                homeworks = new_homework.get('homeworks')
+                if homeworks:
+                    send_message(parse_homework_status(homeworks[0]))
+                current_timestamp = new_homework.get('current_date')
+                time.sleep(1200)  # опрашивать раз в 20 минут
+
+        except KeyboardInterrupt:
+            finish = input(
+                'Вы действительно хотите прервать работу бота? Y/N: '
+                )
+            if finish == 'Y' or finish == "y":
+                print('До встречи!')
+                break
+            elif finish == 'N' or finish == "n":
+                print('Продолжаем работать!')
 
         except Exception as e:
             print(f'Бот упал с ошибкой: {e}')
             time.sleep(5)
-            continue
 
 
 if __name__ == '__main__':
